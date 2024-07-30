91542cf0091b4261635ba4a6fe07b8e6c7621eb4
rocketgithub <rodrigo@aquih.com>
2024-07-30 10:00:40 -0600

    Ya no usar openerp

--- models/invoice.py
+++ models/invoice.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 
-from openerp import models, fields, api, _
-from openerp.exceptions import UserError, ValidationError
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
