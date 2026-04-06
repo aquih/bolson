73a34421093439889571d43f9241cd94ade5d9ce
rocketgithub <rodrigo@aquih.com>
2026-04-06 14:01:36 -0600

    Version 19.0

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name' : 'Bolson',
-    'version' : '2.2',
+    'version' : '2.3',
     'category': 'Custom',
     'description': """Manejo de cajas chicas y liquidaciones ( obsoleto, ya no usar )""",
     'author': 'aquíH',
